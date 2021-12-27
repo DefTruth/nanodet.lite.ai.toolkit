0b587d51ca22d84f4f76e0c925ee544fc35484b7
DefTruth <qiustudent_r@163.com>
2021-12-28 00:34:05 +0800

    Add MNN/TNN/NCNN/ONNXRuntime C++ for NanoDetPlus

--- examples/test_lite_nanodet.cpp
+++ examples/test_lite_nanodet.cpp
@@ -20,9 +20,31 @@ static void test_nanodet()
 
   cv::imwrite(save_img_path, img_bgr);
 
-  std::cout << "Detected Boxes Num: " << detected_boxes.size() << std::endl;
+  std::cout << "NanoDet Detected Boxes Num: " << detected_boxes.size() << std::endl;
 
   delete nanodet;
+
+}
+
+static void test_nanodet_plus()
+{
+  std::string onnx_path = "../hub/onnx/cv/nanodet-plus-m-1.5x_416.onnx";
+  std::string test_img_path = "../resources/9.jpg";
+  std::string save_img_path = "../logs/9_plus.jpg";
+
+  auto *nanodet_plus = new lite::cv::detection::NanoDetPlus(onnx_path);
+
+  std::vector<lite::types::Boxf> detected_boxes;
+  cv::Mat img_bgr = cv::imread(test_img_path);
+  nanodet_plus->detect(img_bgr, detected_boxes);
+
+  lite::utils::draw_boxes_inplace(img_bgr, detected_boxes);
+
+  cv::imwrite(save_img_path, img_bgr);
+
+  std::cout << "NanoDetPlus Detected Boxes Num: " << detected_boxes.size() << std::endl;
+
+  delete nanodet_plus;
 
 }
 
@@ -30,5 +52,6 @@ static void test_nanodet()
 int main(__unused int argc, __unused char *argv[])
 {
   test_nanodet();
+  test_nanodet_plus();
   return 0;
 }
